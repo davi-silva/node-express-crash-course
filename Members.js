7f4871acae58a37ffaa8e7abcab0dec2fcd1ee8e
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-08-30 22:46:52 -0300

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Davi Silva',
+        email: 'davi@davi.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Marshall',
+        email: 'marshall@email.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Darien Lyons',
+        email: 'darien@email.com',
+        status: 'active'
+    },
+]
+
+module.exports = members
